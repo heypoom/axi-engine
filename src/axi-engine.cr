88d0b9d91050c27da6c4cf8bc3e7434ce4370301
Phoomparin Mano <work@phoom.in.th>
2018-02-22 10:40:53 +0700

    :sparkles: Added YAML Parser API

--- src/axi-engine.cr
+++ src/axi-engine.cr
@@ -1,14 +1,18 @@
 require "raze"
 require "yaml"
-require "json"
+require "colorize"
 
 post "/api/create" do |ctx|
   ctx.response.content_type = "application/json"
+  body = ctx.request.body.as(IO).gets_to_end
 
-  raw_body = ctx.request.body.as(IO).gets_to_end
-  data = YAML.parse_all(raw_body)
+  data = YAML.parse_all(body)
 
-  services = data.map {|item| item["name"].to_s}
+  print "YAML: ".colorize(:blue)
+
+  services = data
+    .map {|item| {item["name"].to_s => item["kind"].to_s}}
+    .reduce {|x, y| x.merge(y)}
 
   {data: "Created!", services: services}.to_json
 end
